21637cdf74e6465968a2ebd03c8a5d2f629edc48
liling-kongbai <kong4826@qq.com>
2025-01-13 20:06:58 +0800

    录音功能完成

--- VoiceWakeUpEngine/record_background.py
+++ VoiceWakeUpEngine/record_background.py
@@ -1,18 +1,24 @@
-from ceshi import read_config
-from audio_preprocessing import collect, record
+from config import read_config
+import os
+from audio_preprocessing import count, record
 import time
 
+config = read_config()
+background_dataset_address = config.get('address', 'background_dataset_address')
+background_dataset_address = os.path.join(os.path.dirname(os.path.abspath(__file__)), background_dataset_address)
+record_background_time = config.get('record_time', 'record_background_time')
+samplerate = config.get('audio', 'samplerate')
+channels = config.get('audio', 'channels')
+record_background_time = int(record_background_time)
+samplerate = int(samplerate)
+channels = int(channels)
 
-cf = read_config()
-background_dataset_address = cf('address', 'wakeword_dataset_address')
-record_background_record_time = cf('record_time', 'record_background_record_time')
+background_dataset_count = count(background_dataset_address)
+print(f'当前background音频数据集的数量有{background_dataset_count}条。')
 
-
-background_list = collect(background_dataset_address)
-print(f'当前background音频数量：{len(background_list)}')
-
-record_time = int(input('请输入要录制的音频数量：'))
-for i in range(record_time):
-    input(f'第{i + 1}条， 回车开始……')
-    # 聪明的功能，手动跳过
-    record(f'{background_dataset_address}/{int(time.time())}.wav', record_background_record_time)
+record_count = int(input('请输入要录制的音频数量：'))
+for i in range(record_count):
+    input(f'现在录制第{i + 1}条，请回车开始！')
+    record(record_background_time, samplerate, channels, os.path.join(background_dataset_address, str(int(time.time())) + '.wav'))
+    if i + 1  == record_count:
+        print('录制结束。')
