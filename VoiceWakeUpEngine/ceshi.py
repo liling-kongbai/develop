e58c1f22d435681e66dac23a4c29e4a525f31089
liling-kongbai <kong4826@qq.com>
2025-01-16 15:16:15 +0800

    音频数据合成功能

--- VoiceWakeUpEngine/ceshi.py
+++ VoiceWakeUpEngine/ceshi.py
@@ -1,49 +1,19 @@
-from config import readConfig
-from audio_preprocessing import filenameList, overlop
-import random
-import os
-import time
+import re
 
-config = readConfig()
+# 示例文件名
+file_name = 'C:\\Users\\kongbai\\study\\develop\\VoiceWakeUpEngine\\dataset\\test\\1736996407_0.wav'
+# 定义正则表达式模式
+pattern = re.compile(r'_(\d+)\.wav$')
 
-
-address_choice = int(input('请输入要合成音频的数据集（1，训练集；2，测试集）：'))
-if address_choice == 1:
-    dataset_address = config.get('address', 'train_dataset_address')
-elif address_choice == 2:
-    dataset_address = config.get('address', 'test_dataset_address')
-
-
-overlop_count = int(input('请输入要合成的音频数量：'))
-
-
-
-wakeword_dataset_address = config.get('address', 'wakeword_dataset_address')
-no_wakeword_dataset_address = config.get('address', 'no_wakeword_dataset_address')
-background_dataset_address = config.get('address', 'background_dataset_address')
-
-project_address = os.path.dirname(os.path.abspath(__file__))
-
-
-wakeword_filename_list = filenameList(os.path.join(project_address, wakeword_dataset_address))
-no_wakeword_filename_list = filenameList(os.path.join(project_address, no_wakeword_dataset_address))
-background_filename_list = filenameList(os.path.join(project_address, background_dataset_address))
-
-
-for i in range(overlop_count):
-    probability = random.random()
-    if probability > 0.5:
-        feature_audio_filename = random.choices(wakeword_filename_list)[0]
-        feature_audio_address = os.path.join(project_address, wakeword_dataset_address, feature_audio_filename)
+# 使用正则表达式提取标签
+def extract_label_from_filename(filename):
+    match = pattern.search(filename)
+    # ?????
+    if match:
+        return int(match.group(1))
     else:
-        feature_audio_filename = random.choices(no_wakeword_filename_list)[0]
-        feature_audio_address = os.path.join(project_address, no_wakeword_dataset_address, feature_audio_filename)
+        raise ValueError(f"文件名 {filename} 不符合预期格式")
 
-
-
-    background_audio_filename = random.choices(background_filename_list)[0]
-    background_audio_address = os.path.join(project_address, background_dataset_address, background_audio_filename)
-
-
-
-    overlop(background_audio_address, feature_audio_address, os.path.join(project_address, dataset_address, str(int(time.time())) + '.wav'))
+# 示例使用
+label = extract_label_from_filename(file_name)
+print(f"文件 {file_name} 的标签是: {label}")
